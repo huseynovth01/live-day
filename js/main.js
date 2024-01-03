94007b1498ecb91bc97b9bf595f11e42f7888c11
mac book pro <macbookpro@macs-MacBook-Pro.local>
2024-01-03 11:43:32 +0400

    start

---
+++ js/main.js
@@ -0,0 +1,30 @@
+// 1ci hell
+
+var today = new Date();
+var dd = today.getDate();
+
+var mm = today.getMonth()+1; 
+var yyyy = today.getFullYear();
+if(dd<10) 
+{
+    dd='0'+dd;
+} 
+
+if(mm<10) 
+{
+    mm='0'+mm;
+} 
+today = mm+'-'+dd+'-'+yyyy;
+console.log(today);
+today = mm+'/'+dd+'/'+yyyy;
+console.log(today);
+today = dd+'-'+mm+'-'+yyyy;
+console.log(today);
+today = dd+'/'+mm+'/'+yyyy;
+console.log(today);
+
+
+// 2 ci hell
+
+const today = new Date().toLocaleDateString('en-US');
+console.log(today);
